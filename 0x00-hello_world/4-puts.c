53832f320ad473c46b62b0d8a8f4d7ea70111155
sylviaokorie <sylviangokorie@gmail.com>
2022-09-08 16:53:58 +0100

    my first hello with alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*my first hello word with alx */
+
+int main(void)
+{
+	/* initializing strings */
+	
+	char str1[] = "Programming is like building a multilingual puzzle";
+	/* writing to stdout */
+	puts(str1);
+
+	return (0);
+}
