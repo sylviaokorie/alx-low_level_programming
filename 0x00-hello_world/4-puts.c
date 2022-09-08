ca037afeedcc482e79fed47d2fba0cd7efc92399
sylviaokorie <sylviangokorie@gmail.com>
2022-09-08 18:02:34 +0100

    my first alx c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*my first hello word with alx */
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/* initializing strings */
-	
-	char str1[] = "Programming is like building a multilingual puzzle";
-	/* writing to stdout */
-	puts(str1);
-
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
