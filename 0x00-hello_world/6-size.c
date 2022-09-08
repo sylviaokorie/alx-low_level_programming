6da297e16a7906931ce2ac879b2b17a46db34828
sylviaokorie <sylviangokorie@gmail.com>
2022-09-08 22:42:35 +0100

    size of type variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	float e;
 
 	/* sizeof evalutes the size of a variable */
-	printf("size of a char: %zu byte(s)\n", sizeof(a));
-	printf("size of an int: %zu byte(s)\n", sizeof(b));
-	printf("size of a long int: %zu byte(s)\n", sizeof(c));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("size of a float: %zu byte(s)\n", sizeof(e));
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 return (0);
 }
