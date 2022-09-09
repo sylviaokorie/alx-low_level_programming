69e59f2d143e7955ba2977997f3581fc6a130e04
sylviaokorie <sylviangokorie@gmail.com>
2022-09-09 23:11:12 +0100

    ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for single number using the ASCII
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; ++num)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+return (0);
+}
