66f4f6221c412569b92b1424b799f20090e34f95
sylviaokorie <sylviangokorie@gmail.com>
2022-09-14 00:17:53 +0100

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int m;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (m = 0; m < 48; m++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
