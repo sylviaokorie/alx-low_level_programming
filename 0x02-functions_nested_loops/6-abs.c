5db51f9f7307135e9d9cda802a72dc675e499fca
sylviaokorie <sylviangokorie@gmail.com>
2022-09-13 19:45:00 +0100

    absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - show the absolute value of an integer
+ * @c: varible of int
+ * Return: 0
+ *
+ */
+int _abs(int c)
+{
+	if (c >= 48 && c == 48)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
