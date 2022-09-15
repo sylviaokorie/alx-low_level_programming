d9baba8274f7d630ace885e5d7ff5d6f82943a0d
sylviaokorie <sylviangokorie@gmail.com>
2022-09-15 21:51:40 +0100

    more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,8 +16,8 @@ void more_numbers(void)
 		{
 			if (m >= 10)
 			{
-				_putchar((m / 10) + 0);
-				_putchar((m % 10) + 0);
+				_putchar((m / 10) + 48);
+				_putchar((m % 10) + 48);
 			}
 		}
 		_putchar('\n');
