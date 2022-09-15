355a122e7040ea1a85d8a5aaed8bdf307816f38e
sylviaokorie <sylviangokorie@gmail.com>
2022-09-15 22:23:10 +0100

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ *@n: integer value
+ * Return: 0
+ *
+ */
+void print_diagonal(int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		_putchar(32);
+	}
+	_putchar('\n');
+}
