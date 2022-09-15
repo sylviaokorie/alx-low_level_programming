4d5eb459f8fa31ec2c3fce5dc1e27ebd3e156b7b
sylviaokorie <sylviangokorie@gmail.com>
2022-09-15 22:12:50 +0100

    straight

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ * Return: 0
+ *
+ */
+void print_line(int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
