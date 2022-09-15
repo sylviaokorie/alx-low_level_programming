164641041e25ce94349ed53105a875f563a3788d
sylviaokorie <sylviangokorie@gmail.com>
2022-09-15 21:48:16 +0100

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 10x
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 15; m++)
+		{
+			if (m >= 10)
+			{
+				_putchar((m / 10) + 0);
+				_putchar((m % 10) + 0);
+			}
+		}
+		_putchar('\n');
+	}
+}
