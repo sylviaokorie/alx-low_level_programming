843c6fd303982dd18302385e645341d8556f0de7
sylviaokorie <sylviangokorie@gmail.com>
2022-09-19 16:03:59 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - to calculate the length of char
+ * @s: integer value
+ * Return: Always 0
+ *
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; c <= s[c]; c++)
+	{
+
+	}
+	return (c);
+}
+
