afa23e44256bf9e727bbfd59892d5c5f98f0a9fb
sylviaokorie <sylviangokorie@gmail.com>
2022-09-19 21:41:02 +0100

    puts string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - printing each character of a string
+ * @str: integer value
+ * Return: Always 0
+ *
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
