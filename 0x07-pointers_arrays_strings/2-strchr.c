5276b02cdedf8086670bf5fad4ce3f0b2743581b
sylviaokorie <sylviangokorie@gmail.com>
2022-09-26 21:48:52 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for the occurrence of char and returns the s
+ * @s: string
+ * @c: character of string
+ * Return: Always 0
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
