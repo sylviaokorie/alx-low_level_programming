53de1e1559f592f8dbd6edafad8b09be57233fae
sylviaokorie <sylviangokorie@gmail.com>
2022-10-04 17:54:55 +0100

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,18 @@
 char *_strdup(char *str)
 {
 	char *dap;
-	int i, j, k;
+	int i, j;
 
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		k++;
+		;
 	}
-	dap = (char *) malloc(sizeof(char) * k);
+	dap = (char *) malloc(sizeof(char) * (i +1));
 	if (dap == NULL)
 		return (NULL);
-	for (j = 0; j <= k; j++)
+	for (j = 0; j <= i; j++)
 	{
 		dap[j] = str[j];
 	}
